1a709740238e2512d1e3755684c8006cdd6806a9
Kettani <kettani.samir@outlook.fr>
2018-12-26 17:54:34 +0100

    Initial commit

---
+++ porn.js
@@ -0,0 +1,9 @@
+function Redirection(){
+    document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+
+setTimeout(function() { if (confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"));
+ {
+Redirection()
+} }, 8000);
+
